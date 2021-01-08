6866fdd65774e8ac54bef955a131c9a83829875b
Jakob Flierl <koppi@users.noreply.github.com>
2021-01-08 14:20:42 +0100

    Update copyright time

--- src/serial-latency-test.c
+++ src/serial-latency-test.c
@@ -1,6 +1,6 @@
 /* Serial Latency Tester
  *
- * (C) 2013  Jakob Flierl <jakob.flierl@gmail.com>
+ * Copyright (C) 2013-2021  Jakob Flierl <jakob.flierl@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
